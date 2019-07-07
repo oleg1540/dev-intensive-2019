4dba3eec0e7ad09eccbd0eabc2dd97e0d4a968c9
Kolesov\Олег <oleg14wo1@yandex.ru>
2019-07-07 22:25:06 +0300

    add stripHtml() extension for String

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -8,3 +8,23 @@ fun String.truncate(length: Int = 16): String {
     }
     return result
 }
+
+fun String.stripHtml(): String {
+    return this.replace("<[^>]*>".toRegex(),"").trim().removeHtmlEscape().removeDoubleWhitespace()
+}
+
+private fun String.removeDoubleWhitespace(): String {
+    var result = ""
+    var prev: Char = 0.toChar()
+    for (word in this) {
+        if (!word.isWhitespace() || prev.compareTo(word) != 0) {
+            result = result.plus(word)
+        }
+        prev = word
+    }
+    return result
+}
+
+private fun String.removeHtmlEscape(): String {
+    return this.replace("[&<>'\"]*".toRegex(), "")
+}
