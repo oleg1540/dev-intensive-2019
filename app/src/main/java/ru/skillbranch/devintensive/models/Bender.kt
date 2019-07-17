4374593ab6f464940a0b1272eaea1f9213da3603
Kolesov\Олег <oleg14wo1@yandex.ru>
2019-07-17 22:20:28 +0300

    fix typos in listenAnswer

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -3,7 +3,7 @@ package ru.skillbranch.devintensive.models
 class Bender(var status: Status = Status.NORMAL, var question: Question = Question.NAME) {
     var errorNum = 0
     enum class Status(val color: Triple<Int, Int, Int>) {
-        NORMAL(Triple(255, 255, 255)),
+        NORMAL(Triple(255, 255, 255)) ,
         WARNING(Triple(255, 120, 0)),
         DANGER(Triple(255, 60, 60)),
         CRITICAL(Triple(255, 0, 0));
@@ -21,7 +21,7 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
         NAME("Как меня зовут?", listOf("бендер", "bender")) {
             override fun nextQuestion(): Question = PROFESSION
         },
-        PROFESSION("Назови мою профессию", listOf("сгибальщик", "bender")) {
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
             override fun nextQuestion(): Question = MATERIAL
         },
         MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")) {
@@ -33,7 +33,7 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
         SERIAL("Мой серийный номер?", listOf("2716057")) {
             override fun nextQuestion(): Question = IDLE
         },
-        IDLE("На этом всё, вопросов больше нет", listOf()) {
+        IDLE("На этом все, вопросов больше нет", listOf()) {
             override fun nextQuestion(): Question = IDLE
         };
 
@@ -52,11 +52,11 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
     fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
         return when {
             question == Question.IDLE -> {
-                "Отлично - это правильный ответ!\n${question.question}" to status.color
+                "Отлично - ты справился!\n${question.question}" to status.color
             }
             question.answers.contains(answer) -> {
                 question = question.nextQuestion()
-                "Отлично - это правильный ответ!\n${question.question}" to status.color
+                "Отлично - ты справился!\n${question.question}" to status.color
             }
             errorNum === 3 -> {
                 errorNum = 0
@@ -67,7 +67,7 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
             else -> {
                 errorNum++
                 status = status.nextStatus()
-                "Это не правильный ответ!\n${question.question}" to status.color
+                "Это неправильный ответ\n${question.question}" to status.color
             }
         }
     }
