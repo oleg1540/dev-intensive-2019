d7ee688f70ae4c6c965516c557768ffe11e3e3aa
Kolesov\Олег <oleg14wo1@yandex.ru>
2019-07-17 21:54:15 +0300

    fix typo hideKeyboatd

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -4,7 +4,7 @@ import android.app.Activity
 import android.content.Context
 import android.view.inputmethod.InputMethodManager
 
-fun Activity.hideKeyboatd() {
+fun Activity.hideKeyboard() {
     val inputManager: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputManager.hideSoftInputFromWindow(currentFocus.windowToken, InputMethodManager.SHOW_FORCED)
 }
